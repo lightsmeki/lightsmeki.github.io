2ad5865b999c093c0e8b34cfb22af509c921502a
lightsmeki <371200212@qq.com>
2016-07-06 17:06:24 +0800

    201607062

---
+++ js/header_footer.js
@@ -0,0 +1,10 @@
+(function($) {
+    //
+    //$("#header").html("<div><div id="imglogo"><a href="/"><img src="./img/logo.png" alt="Burin&#39;s blog" title="Burin&#39;s blog"/></a> </div> " +
+    //"<div id="textlogo"> <h1 class="site-name"><a href="/" title="Burin&#39;s blog">Burin&#39;s blog</a></h1> <h2 class="blog-motto">不林的博客  ( ﾟ∀ﾟ)よろしく❤</h2> " +
+    //"</div> <div class="navbar"><a class="navbutton navmobile" href="#" title="菜单"> </a></div> <nav class="animated"> <ul> <ul> <li>" +
+    //"<a href="./">首页</a></li> <li><a href="./archives">归档</a></li> <li><a href="./about">关于</a></li> <li> <form class="search" action="//google.com/search"
+    //method="get" accept-charset="utf-8"> <label>Search</label> <input type="search" id="search" name="q" autocomplete="off" maxlength="20"
+    //placeholder="Search" /> <input type="hidden" name="q" value="site:lightsmeki.github.io"> </form> </li> </ul> </ul> </nav> </div>");
+
+})(jQuery);
